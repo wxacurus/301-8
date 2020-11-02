8d317e2f8bf66ad52a071d6470ab420ebf09a8ba
wxacurus <67513546+wxacurus@users.noreply.github.com>
2020-11-02 17:42:48 -0500

    Update CityListTest.java

--- app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
@@ -28,4 +28,9 @@ class CityListTest {
         assertEquals(2, cityList.countCities());
         assertTrue(cityList.hasCity(new City("Regina", "Saskatchewan")));
     }
+    
+    @Test
+    void testGetCity(){
+        CityList cityList = mockCityList()
+        assertEquals(0, mockCity.compareTo(cityList.getCities().get(0));
 }
